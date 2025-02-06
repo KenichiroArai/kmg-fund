2a41746c5f04be829f7764417fda0d96cd887ea9
KenichiroArai <sammyleejunior@gmail.com>
2025-02-06 13:42:28 +0900

    Eclipse設定ファイルとKmgFoundationApplicationのエントリポイントを追加

--- src/main/java/kmg/foundation/KmgFoundationApplication.java
+++ src/main/java/kmg/foundation/KmgFoundationApplication.java
@@ -2,11 +2,25 @@ package kmg.foundation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
+/**
+ * KMG基盤アプリケーション
+ */
 @SpringBootApplication
 public class KmgFoundationApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(KmgFoundationApplication.class, args);
+    /**
+     * エントリポイント
+     *
+     * @param args
+     *             引数
+     */
+    public static void main(final String[] args) {
+
+        @SuppressWarnings("resource")
+        final ConfigurableApplicationContext ctx = SpringApplication.run(KmgFoundationApplication.class, args);
+        ctx.close();
+
     }
 }
