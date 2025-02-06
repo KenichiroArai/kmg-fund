3ebd3e1923b7b07645b44f4d8b969fedef08f49c
KenichiroArai <sammyleejunior@gmail.com>
2025-02-06 13:36:54 +0900

    プロジェクトの初期設定ファイルと構成を追加

---
+++ src/main/java/kmg/foundation/KmgFoundationApplication.java
@@ -0,0 +1,12 @@
+package kmg.foundation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmgFoundationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KmgFoundationApplication.class, args);
+    }
+}
