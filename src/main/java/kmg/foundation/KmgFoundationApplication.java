7f4e6ba784a9f6d392695e8836a15620c01ea787
KenichiroArai <sammyleejunior@gmail.com>
2025-02-18 21:54:53 +0900

    pom.xmlとアプリケーションの設定を更新し、依存関係を整理、ログ設定を追加

--- src/main/java/kmg/foundation/KmgFoundationApplication.java
+++ src/main/java/kmg/foundation/KmgFoundationApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import kmg.core.infrastructure.type.KmgString;
+
 /**
  * KMG基盤アプリケーション
  */
@@ -18,9 +20,15 @@ public class KmgFoundationApplication {
      */
     public static void main(final String[] args) {
 
-        @SuppressWarnings("resource")
-        final ConfigurableApplicationContext ctx = SpringApplication.run(KmgFoundationApplication.class, args);
-        ctx.close();
+        try (final ConfigurableApplicationContext ctx = SpringApplication.run(KmgFoundationApplication.class, args)) {
+
+            // テスト処理を実行
+            KmgString str = new KmgString("TestAbc5Ttt");
+            System.out.println(str.toCamelCase());
+
+            System.out.println("HelloWorld");
+
+        }
 
     }
 }
