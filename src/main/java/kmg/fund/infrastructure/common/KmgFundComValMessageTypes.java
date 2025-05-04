8a6b54b8b7294769a3adb3f18e05540fd246e398
KenichiroArai <sammyleejunior@gmail.com>
2025-05-05 00:36:52 +0900

    KmgFundValMessageTypesおよびKmgFundComValMessageTypesインタフェースを新規作成し、基盤バリデーションメッセージの種類を定義しました。

---
+++ src/main/java/kmg/fund/infrastructure/common/KmgFundComValMessageTypes.java
@@ -0,0 +1,18 @@
+package kmg.fund.infrastructure.common;
+
+import kmg.core.infrastructure.common.KmgComValMessageTypes;
+
+/**
+ * KMG 基盤バリデーションメッセージの種類のインタフェース
+ *
+ * @author KenichiroArai
+ *
+ * @since 0.1.0
+ *
+ * @version 0.1.0
+ */
+public interface KmgFundComValMessageTypes extends KmgFundComMessageTypes, KmgComValMessageTypes {
+
+    // 処理なし
+
+}
