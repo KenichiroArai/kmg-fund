659fb4375cedd47bf427196855b3a04ad2f14f55
KenichiroArai <sammyleejunior@gmail.com>
2025-05-06 00:55:00 +0900

    KmgFundValExceptionクラスを新規作成し、KMG基盤バリデーション例外を実装しました

---
+++ src/main/java/kmg/fund/infrastructure/exception/KmgFundValException.java
@@ -0,0 +1,54 @@
+package kmg.fund.infrastructure.exception;
+
+import kmg.core.infrastructure.exception.KmgValidationException;
+import kmg.core.infrastructure.model.validation.KmgValidationsModel;
+
+/**
+ * KMG 基盤バリデーション例外<br>
+ *
+ * @author KenichiroArai
+ *
+ * @since 0.1.0
+ *
+ * @version 0.1.0
+ */
+public class KmgFundValException extends KmgValidationException {
+
+    /**
+     * デフォルトシリアルバージョンＵＩＤ
+     *
+     * @since 0.1.0
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * コンストラクタ<br>
+     *
+     * @since 0.1.0
+     *
+     * @param validationsModel
+     *                         KMGバリデーション集合モデル
+     */
+    public KmgFundValException(final KmgValidationsModel validationsModel) {
+
+        this(validationsModel, null);
+
+    }
+
+    /**
+     * コンストラクタ<br>
+     *
+     * @since 0.1.0
+     *
+     * @param validationsModel
+     *                         KMGバリデーション集合モデル
+     * @param cause
+     *                         原因
+     */
+    public KmgFundValException(final KmgValidationsModel validationsModel, final Throwable cause) {
+
+        super(validationsModel, cause);
+
+    }
+
+}
